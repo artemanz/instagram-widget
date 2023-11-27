90e73939f7b6dad104227626b8009658d4e80bbd
Artem Anzin <anzinartix@gmail.com>
2023-11-27 18:00:23 +0300

    v1.1 Update widget

--- src/shared/widget.js
+++ src/shared/widget.js
@@ -1,5 +1,5 @@
 function instagramWidget() {
-  const apiUrl = "https://weblab420.com/widget/v1/api/simple";
+  const apiUrl = process.env.NEXT_PUBLIC_API;
 
   const FollowSvg = `
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
